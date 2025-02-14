e66bc7d8dc6c16369e1e3cc1c60b34e1bb784bc9
Nikolaj Schlej <schlej@live.de>
2025-02-14 05:22:05 +0100

    Apply a small patch to common/zlib/gzguts.h to fix a build issue in macOS

--- common/zlib/gzguts.h
+++ common/zlib/gzguts.h
@@ -3,6 +3,11 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
+// UEFITool: required for macOS
+#ifndef _WIN32
+  #include <unistd.h>
+#endif
+
 #ifdef _LARGEFILE64_SOURCE
 #  ifndef _LARGEFILE_SOURCE
 #    define _LARGEFILE_SOURCE 1
